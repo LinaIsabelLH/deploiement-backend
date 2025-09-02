c256e1a50d7d780a181586ac7fc773be0711b26d
LinaIsabelLH <londonohidalgo@gmail.com>
2025-09-02 17:32:03 +0200

    test deploiement render2 variables env

--- project/settings.py
+++ project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ty77=+%7&p0%$okc=96z!w@75fkjwilvqpi!bpkij@^)rhg_wh'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
